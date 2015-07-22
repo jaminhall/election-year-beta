243a652162006f1835855f6e6dc8415b4b28958d
jaminhall <jamin.hall@hotmail.com>
2015-07-21 21:48:53 -0400

    Commented out the remove all players code

--- app.js
+++ app.js
@@ -16,97 +16,99 @@ app.get("/", function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-            socket.on('new player', function (data, callback) {
-                if (!(data in players)) {
-                    callback(true);
-                    socket.player = data;
-                    players[socket.player] = socket;
-                    updatePlayers();
-                    updateGames();
-                } else {
-                    callback(false);
-                }
-            });
+    socket.on('new player', function (data, callback) {
+        if (!(data in players)) {
+            callback(true);
+            socket.player = data;
+            players[socket.player] = socket;
+            updatePlayers();
+            updateGames();
+        } else {
+            callback(false);
+        }
+    });
 
-            socket.on('new game', function (data, callback) {
-                if (!(data in games)) {
-                    callback(true);
-                    socket.game = Object.create(game);
-                    socket.game.name = data;
-                    socket.game.players = [];
-                    socket.game.settings = game.settings;
+    socket.on('new game', function (data, callback) {
+        if (!(data in games)) {
+            callback(true);
+            socket.game = Object.create(game);
+            socket.game.name = data;
+            socket.game.players = [];
+            socket.game.settings = game.settings;
 
-                    games[data] = {
-                        socket: socket,
-                        game: socket.game
-                    };
-                    socket.join(data);
-                    updateGames();
-                    addPlayerToGame(data);
-                } else {
-                    callback(false);
-                }
-            });
+            games[data] = {
+                socket: socket,
+                game: socket.game
+            };
+            socket.join(data);
+            updateGames();
+            addPlayerToGame(data);
+        } else {
+            callback(false);
+        }
+    });
 
-            socket.on('join game', function (data, callback) {
-                if (data in games) {
-                    socket.join(data);
-                    addPlayerToGame(data);
-                    callback(true);
-                } else {
-                    console.log("Couldn't find " + data);
-                    callback(false);
-                }
-            });
+    socket.on('join game', function (data, callback) {
+        if (data in games) {
+            socket.join(data);
+            addPlayerToGame(data);
+            callback(true);
+        } else {
+            console.log("Couldn't find " + data);
+            callback(false);
+        }
+    });
 
-            socket.on('cancel game', function (data) {
-                    if (games[data]) {
-                        cancelGame(data);
-                        delete games[data];
-                    }
-                });
+    socket.on('cancel game', function (data) {
+        if (games[data]) {
+            cancelGame(data);
+            delete games[data];
+        }
+    });
 
-                socket.on("disconnect", function (data) {
-                    if (!socket.player) return;
-                    delete players[socket.player];
-                    if (games[socket.player]) {
-                        cancelGame(socket.player);
-                    }
-                    delete games[socket.player];
-                    updatePlayers();
-                    updateGames();
-                });
+    socket.on("disconnect", function (data) {
+        if (!socket.player) return;
+        delete players[socket.player];
+        if (games[socket.player]) {
+            cancelGame(socket.player);
+        }
+        delete games[socket.player];
+        updatePlayers();
+        updateGames();
+    });
 
-                socket.on('start game', function (data) {
-                    //Shuffle all decks
-                    //Pass out cards (1 candidate card, 3 action cards)
-                    //Determine playing order
-                    //Update all players in the room
-                    //Start player 1's turn
-                });
+    socket.on('start game', function (data) {
+        //Shuffle all decks
+        //Pass out cards (1 candidate card, 3 action cards)
+        //Determine playing order
+        //Update all players in the room
+        //Start player 1's turn
+    });
 
-                function cancelGame(data) {
-                    console.log("Cancel " + data + " game");
-                    io.to(data).emit("cancelGame");
-                   //Disconnect all players from the room
-                    io.sockets.clients(data).forEach(function (s) {
-                        s.leave(data);
-                    });
-                }
+    function cancelGame(data) {
+        console.log("Cancel " + data + " game");
+        io.to(data).emit("cancelGame");
+        //Disconnect all players from the room
+        /*
+        io.sockets.clients(data).forEach(function (s) {
+            s.leave(data);
+        });
+        */
+    }
 
-                function updatePlayers() {
-                    io.sockets.emit("players", Object.keys(players));
-                }
+    function updatePlayers() {
+        io.sockets.emit("players", Object.keys(players));
+    }
 
-                function updateGames() {
-                    io.sockets.emit("games", Object.keys(games));
-                }
+    function updateGames() {
+        io.sockets.emit("games", Object.keys(games));
+    }
 
-                function addPlayerToGame(data) {
-                    games[data].game.players.push(socket.player);
-                    //console.log("*******************");
-                    //console.log(games[data].game);
-                    //console.log("*******************");
-                    io.to(data).emit("playerAddedToGame", games[data].game);
-                }
-            });
+    function addPlayerToGame(data) {
+        games[data].game.players.push(socket.player);
+        //console.log("*******************");
+        //console.log(games[data].game);
+        //console.log("*******************");
+        io.to(data).emit("playerAddedToGame", games[data].game);
+    }
+});
